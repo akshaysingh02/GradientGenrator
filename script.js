73aceb04d10f0e57519af3f0500d7292d296a03f
Akshay singh <65339543+akshaysingh02@users.noreply.github.com>
2020-07-04 13:17:44 +0530

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ function setGradient() {
   body.style.background =
     "linear-gradient(to right," + c1.value + "," + c2.value + ")";
   ran.style.background =
-    "linear-gradient(to top," + c2.value + "," + c1.value + ")";
+    "linear-gradient(to right," + c2.value + "," + c1.value + ")";
   cs.textContent = body.style.background;
 }
 c1.addEventListener("input", setGradient);
@@ -20,7 +20,7 @@ function randomhex() {
   var rgb2 = '#' + (Math.random().toString(16) + "000000").substring(2,8);
 
   body.style.background = "linear-gradient(to right," + rgb1 + "," + rgb2 + ")";
-  ran.style.background = "linear-gradient(to top," + rgb2 + "," + rgb1 + ")";
+  ran.style.background = "linear-gradient(to right," + rgb1 + "," + rgb2 + ")";
   cs.textContent = body.style.background;
   c1.value = rgb1;
   c2.value = rgb2;
