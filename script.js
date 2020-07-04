d61294a9d6f30e6328e5a4eb23a61bbc7bd27d1f
Akshay singh <65339543+akshaysingh02@users.noreply.github.com>
2020-07-04 13:05:36 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,24 +2,28 @@ var cs = document.querySelector("h3");
 var c1 = document.querySelector(".color1");
 var c2 = document.querySelector(".color2");
 var body = document.getElementById("gradient");
-var random = document.querySelector("button");
+var ran = document.querySelector("button");
 
 
 function setGradient() {
   body.style.background =
     "linear-gradient(to right," + c1.value + "," + c2.value + ")";
-    cs.textContent = body.style.background;
+  ran.style.background =
+    "linear-gradient(to top," + c2.value + "," + c1.value + ")";
+  cs.textContent = body.style.background;
 }
 c1.addEventListener("input", setGradient);
 c2.addEventListener("input", setGradient);
 
+function randomhex() {
+  var rgb1 ='#' + (Math.random().toString(16) + "000000").substring(2,8);
+  var rgb2 = '#' + (Math.random().toString(16) + "000000").substring(2,8);
 
-
-
-function randomGradient(){
-    body.style.background =
-    "linear-gradient(to right," + random() + "," + random() + ")";
-    cs.textContent = body.style.background;
+  body.style.background = "linear-gradient(to right," + rgb1 + "," + rgb2 + ")";
+  ran.style.background = "linear-gradient(to top," + rgb2 + "," + rgb1 + ")";
+  cs.textContent = body.style.background;
+  c1.value = rgb1;
+  c2.value = rgb2;
 }
 
-random.addEventListener("click", randomGradient);
+ran.addEventListener("click", randomhex);
